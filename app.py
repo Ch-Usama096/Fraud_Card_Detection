ccb51c21d6f7d5aad494084e7fdc11185abac012
Usama Husnain <usamahusnain096@gmail.com>
2024-07-17 08:12:02 -0600

    Edit the Some code from app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import the important Modules
-from src.Fraud_Card_Detection.logger import logging
+from Fraud_Card_Detection.logger import logging
 
 if __name__ == "__main__":
-    logging.info("This is the Message for the Logs")
+    logging.info("This is the Message for the Logging")
